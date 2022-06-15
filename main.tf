2bccc9b5546751d908c6cda2d759b1c054db9744
Pouria Shahmohammadi <53808802+mstiesto@users.noreply.github.com>
2022-06-15 14:54:43 +0430

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATOOPX4TUIHJBJ2ND"
-  secret_key = "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"
+  access_key = "xxxx"
+  secret_key = "xxxx"
 }
 
 module "IAM" {
